9ab23a4b0de698866498e74716ed6bfa13e9d386
osagie eboigbe <osagie_eboigbe>
2020-12-07 13:23:42 +0100

    Change file

--- app1.py
+++ app1.py
@@ -6,14 +6,15 @@ pickle_in = open("grid2.pkl","rb")
 classifier=joblib.load(pickle_in)#step 2
 cv_model = open('vectorizer.pkl', 'rb')
 cv = joblib.load(cv_model)
-#logit=joblib.load('./logit1.pkl')
-def predict(data):
-    #data=[int_features]
-    vect =cv.transform(data)
-    #vect=vect.toarray()
 
-    predictions = classifier.predict(vect)[0]
+def predict(data):
+
+    vect =cv.transform(data)
+
+
+    predictions = classifier.predict(vect)
     return predictions
+
 def main ():
     from PIL import Image
     image = Image.open('images.jpg')
@@ -37,27 +38,34 @@ def main ():
     st.title("Prediction of spam App")
 
     if add_selectbox == 'Online':
-
         url = st.text_input('Words(Please input  words)')
 
-    output=""
-    data= [url]
-    if st.button("Predict"):
-        output = predict(data)
-    if output==1:
-        output='ham'
-    if output==0:
-        output='spam'
+        output=""
+        data=[url]
 
-    st.success(' output is {}'.format(output))
+
+        if st.button("Predict"):
+            output = predict(data)
+        if output==1:
+            output='Nonspam'
+        if output==0:
+            output='spam'
+
+
+
+
+
+        st.success(' output is {}'.format(output))
 
     if add_selectbox == 'Batch':
-
         file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
 
         if file_upload is not None:
-            data = pd.read_csv(file_upload)
-            predictions = predict(data)
+            data = pd.read_csv(file_upload,  encoding = 'latin-1')
+
+
+            predictions = predict1(data)
+            #predition.replace({1:'Nonspam',0:'spam'},inplace=True)
             st.write(predictions)
 if __name__ == '__main__':
     main()
