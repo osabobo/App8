0f2a5ce41e88251fdb734783d38c42cafb069b1e
osagie eboigbe <36449465+osabobo@users.noreply.github.com>
2023-04-05 13:08:44 +0100

    change5

--- app1.py
+++ app1.py
@@ -3,81 +3,64 @@ import pandas as pd
 import joblib
 from io import StringIO
 import streamlit as st
-pickle_in = open("grid2.pkl","rb")
-classifier=joblib.load(pickle_in)#step 2
+
+pickle_in = open("grid2.pkl", "rb")
+classifier = joblib.load(pickle_in)
 cv_model = open('vectorizer.pkl', 'rb')
 cv = joblib.load(cv_model)
 
+
 def predict(data):
-
-    vect =cv.transform(data)
-
-
+    vect = cv.transform(data)
     predictions = classifier.predict(vect)[0]
     return predictions
+
+
 def predict1(data1):
-
-    vect =cv.transform(data1['v2'])
-    vect=vect.toarray()
-
+    vect = cv.transform(data1['v2'])
+    vect = vect.toarray()
     predictions = classifier.predict(vect)
     return predictions
-def main ():
+
+
+def main():
     from PIL import Image
     image = Image.open('images.jpg')
     image_spam = Image.open('index.jpg')
 
-    st.image(image,use_column_width=False)
+    st.image(image, use_column_width=False)
 
     add_selectbox = st.sidebar.selectbox(
-    "How would you like to predict?",
-    ("Online", "Batch"))
+        "How would you like to predict?", ("Online", "Batch"))
 
     st.sidebar.info('This app checks if an SMS is a spam or not')
-
-
     st.sidebar.image(image_spam)
-
-
-
-
 
     st.title("Spam SMS Detection")
 
     if add_selectbox == 'Online':
         url = st.text_input('Input the SMS')
-
-        output=""
-        data=[url]
-
+        output = ""
+        data = [url]
 
         if st.button("Predict"):
             output = predict(data)
-        if output==1:
-            output='This SMS is not a spam'
-        if output==0:
-            output='This SMS is a spam'
-
-
-
-
+        if output == 1:
+            output = 'This SMS is not a spam'
+        if output == 0:
+            output = 'This SMS is a spam'
 
         st.success(output)
 
     if add_selectbox == 'Batch':
-        #st.set_option('deprecation.showfileUploaderEncoding', False)
-        #file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"],encoding =None, key = 'a')
-
-
-
         st.title('Make sure the csv File is in the same format  as spam.csv before uploading to avoid Error')
         uploaded_file = st.file_uploader("Choose a CSV file", type="csv", encoding="utf-8")
-        if file_upload is not None:
-            data1 = pd.read_csv(file_upload,encoding = 'latin-1')
+        if uploaded_file is not None:
+            data1 = pd.read_csv(uploaded_file, encoding='latin-1')
             predictions = predict1(data1)
             st.write(predictions)
 
 
-
 if __name__ == '__main__':
     main()
+
